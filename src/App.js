19e7aa78de90fc3b6b1f51d7ed583ea4a301eb20
matgus217 <>
2024-02-09 07:47:59 +0000

    Add app and styles in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "./App.css";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+  
+    const addTodo = () => {
+      if (todo !== "") {
+        setTodos([...todos, todo]);
+        setTodo("");
+      }
+    };
+  
+    const deleteTodo = (text) => {
+      const newTodos = todos.filter((todo) => {
+        return todo !== text;
+      });
+      setTodos(newTodos);
+    };
+  
+    return (
+      <div className="App">
+        <h1>What's on the schedule today?</h1>
+  
+        <div className="input-wrapper">
+          <input
+            type="text"
+            name="todo"
+            value={todo}
+            placeholder="Add your scheduled items"
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <button className="add-button" onClick={addTodo}>
+            Add
+          </button>
+        </div>
+  
+        {todos?.length > 0 ? (
+          <ul className="todo-list">
+            {todos.map((todo, index) => (
+              <div className="todo">
+                <li key={index}> {todo} </li>
+  
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteTodo(todo);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">
+            <p>The schedule is empty</p>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default App;
